b5e7d96ec2b02eb1a5a29cbdaf89b970c7e9fcfa
MizukiSonoko <mizuki.sonoko@gmail.com>
2017-04-03 13:51:55 +0900

    Update flatbuffers scheme

--- sample_generated.h
+++ sample_generated.h
@@ -9,20 +9,20 @@
 namespace sample {
 
 struct Object1;
-struct Object1T;
 
 struct Object2;
-struct Object2T;
+
+struct Object3;
 
 struct SampleRoot;
-struct SampleRootT;
 
 enum Object {
   Object_NONE = 0,
   Object_Object1 = 1,
   Object_Object2 = 2,
+  Object_Object3 = 3,
   Object_MIN = Object_NONE,
-  Object_MAX = Object_Object2
+  Object_MAX = Object_Object3
 };
 
 inline const char **EnumNamesObject() {
@@ -30,6 +30,7 @@ inline const char **EnumNamesObject() {
     "NONE",
     "Object1",
     "Object2",
+    "Object3",
     nullptr
   };
   return names;
@@ -52,56 +53,14 @@ template<> struct ObjectTraits<Object2> {
   static const Object enum_value = Object_Object2;
 };
 
-struct ObjectUnion {
-  Object type;
-  flatbuffers::NativeTable *table;
-
-  ObjectUnion() : type(Object_NONE), table(nullptr) {}
-  ObjectUnion(ObjectUnion&& u):
-    type(Object_NONE), table(nullptr)
-    { std::swap(type, u.type); std::swap(table, u.table); }
-  ObjectUnion(const ObjectUnion &);
-  ObjectUnion &operator=(const ObjectUnion &);
-  ~ObjectUnion() { Reset(); }
-
-  void Reset();
-
-  template <typename T>
-  void Set(T&& value) {
-    Reset();
-    type = ObjectTraits<typename T::TableType>::enum_value;
-    if (type != Object_NONE) {
-      table = new T(std::forward<T>(value));
-    }
-  }
-
-  static flatbuffers::NativeTable *UnPack(const void *obj, Object type, const flatbuffers::resolver_function_t *resolver);
-  flatbuffers::Offset<void> Pack(flatbuffers::FlatBufferBuilder &_fbb, const flatbuffers::rehasher_function_t *_rehasher = nullptr) const;
-
-  Object1T *AsObject1() {
-    return type == Object_Object1 ?
-      reinterpret_cast<Object1T *>(table) : nullptr;
-  }
-  Object2T *AsObject2() {
-    return type == Object_Object2 ?
-      reinterpret_cast<Object2T *>(table) : nullptr;
-  }
+template<> struct ObjectTraits<Object3> {
+  static const Object enum_value = Object_Object3;
 };
 
 bool VerifyObject(flatbuffers::Verifier &verifier, const void *obj, Object type);
 bool VerifyObjectVector(flatbuffers::Verifier &verifier, const flatbuffers::Vector<flatbuffers::Offset<void>> *values, const flatbuffers::Vector<uint8_t> *types);
 
-struct Object1T : public flatbuffers::NativeTable {
-  typedef Object1 TableType;
-  std::string text;
-  bool boolean;
-  Object1T()
-      : boolean(false) {
-  }
-};
-
 struct Object1 FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
-  typedef Object1T NativeTableType;
   enum {
     VT_TEXT = 4,
     VT_BOOLEAN = 6
@@ -119,9 +78,6 @@ struct Object1 FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
            VerifyField<uint8_t>(verifier, VT_BOOLEAN) &&
            verifier.EndTable();
   }
-  Object1T *UnPack(const flatbuffers::resolver_function_t *_resolver = nullptr) const;
-  void UnPackTo(Object1T *_o, const flatbuffers::resolver_function_t *_resolver = nullptr) const;
-  static flatbuffers::Offset<Object1> Pack(flatbuffers::FlatBufferBuilder &_fbb, const Object1T* _o, const flatbuffers::rehasher_function_t *_rehasher = nullptr);
 };
 
 struct Object1Builder {
@@ -166,19 +122,7 @@ inline flatbuffers::Offset<Object1> CreateObject1Direct(
       boolean);
 }
 
-flatbuffers::Offset<Object1> CreateObject1(flatbuffers::FlatBufferBuilder &_fbb, const Object1T *_o, const flatbuffers::rehasher_function_t *_rehasher = nullptr);
-
-struct Object2T : public flatbuffers::NativeTable {
-  typedef Object2 TableType;
-  std::string text;
-  int32_t integer;
-  Object2T()
-      : integer(0) {
-  }
-};
-
 struct Object2 FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
-  typedef Object2T NativeTableType;
   enum {
     VT_TEXT = 4,
     VT_INTEGER = 6
@@ -196,9 +140,6 @@ struct Object2 FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
            VerifyField<int32_t>(verifier, VT_INTEGER) &&
            verifier.EndTable();
   }
-  Object2T *UnPack(const flatbuffers::resolver_function_t *_resolver = nullptr) const;
-  void UnPackTo(Object2T *_o, const flatbuffers::resolver_function_t *_resolver = nullptr) const;
-  static flatbuffers::Offset<Object2> Pack(flatbuffers::FlatBufferBuilder &_fbb, const Object2T* _o, const flatbuffers::rehasher_function_t *_rehasher = nullptr);
 };
 
 struct Object2Builder {
@@ -243,18 +184,70 @@ inline flatbuffers::Offset<Object2> CreateObject2Direct(
       integer);
 }
 
-flatbuffers::Offset<Object2> CreateObject2(flatbuffers::FlatBufferBuilder &_fbb, const Object2T *_o, const flatbuffers::rehasher_function_t *_rehasher = nullptr);
-
-struct SampleRootT : public flatbuffers::NativeTable {
-  typedef SampleRoot TableType;
-  std::vector<Object> objects_type;
-  std::vector<ObjectUnion> objects;
-  SampleRootT() {
+struct Object3 FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
+  enum {
+    VT_TEXT = 4,
+    VT_MESSAGE = 6
+  };
+  const flatbuffers::String *text() const {
+    return GetPointer<const flatbuffers::String *>(VT_TEXT);
+  }
+  const flatbuffers::String *message() const {
+    return GetPointer<const flatbuffers::String *>(VT_MESSAGE);
+  }
+  bool Verify(flatbuffers::Verifier &verifier) const {
+    return VerifyTableStart(verifier) &&
+           VerifyFieldRequired<flatbuffers::uoffset_t>(verifier, VT_TEXT) &&
+           verifier.Verify(text()) &&
+           VerifyField<flatbuffers::uoffset_t>(verifier, VT_MESSAGE) &&
+           verifier.Verify(message()) &&
+           verifier.EndTable();
   }
 };
 
+struct Object3Builder {
+  flatbuffers::FlatBufferBuilder &fbb_;
+  flatbuffers::uoffset_t start_;
+  void add_text(flatbuffers::Offset<flatbuffers::String> text) {
+    fbb_.AddOffset(Object3::VT_TEXT, text);
+  }
+  void add_message(flatbuffers::Offset<flatbuffers::String> message) {
+    fbb_.AddOffset(Object3::VT_MESSAGE, message);
+  }
+  Object3Builder(flatbuffers::FlatBufferBuilder &_fbb)
+        : fbb_(_fbb) {
+    start_ = fbb_.StartTable();
+  }
+  Object3Builder &operator=(const Object3Builder &);
+  flatbuffers::Offset<Object3> Finish() {
+    const auto end = fbb_.EndTable(start_, 2);
+    auto o = flatbuffers::Offset<Object3>(end);
+    fbb_.Required(o, Object3::VT_TEXT);
+    return o;
+  }
+};
+
+inline flatbuffers::Offset<Object3> CreateObject3(
+    flatbuffers::FlatBufferBuilder &_fbb,
+    flatbuffers::Offset<flatbuffers::String> text = 0,
+    flatbuffers::Offset<flatbuffers::String> message = 0) {
+  Object3Builder builder_(_fbb);
+  builder_.add_message(message);
+  builder_.add_text(text);
+  return builder_.Finish();
+}
+
+inline flatbuffers::Offset<Object3> CreateObject3Direct(
+    flatbuffers::FlatBufferBuilder &_fbb,
+    const char *text = nullptr,
+    const char *message = nullptr) {
+  return sample::CreateObject3(
+      _fbb,
+      text ? _fbb.CreateString(text) : 0,
+      message ? _fbb.CreateString(message) : 0);
+}
+
 struct SampleRoot FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
-  typedef SampleRootT NativeTableType;
   enum {
     VT_OBJECTS_TYPE = 4,
     VT_OBJECTS = 6
@@ -274,15 +267,12 @@ struct SampleRoot FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
            VerifyObjectVector(verifier, objects(), objects_type()) &&
            verifier.EndTable();
   }
-  SampleRootT *UnPack(const flatbuffers::resolver_function_t *_resolver = nullptr) const;
-  void UnPackTo(SampleRootT *_o, const flatbuffers::resolver_function_t *_resolver = nullptr) const;
-  static flatbuffers::Offset<SampleRoot> Pack(flatbuffers::FlatBufferBuilder &_fbb, const SampleRootT* _o, const flatbuffers::rehasher_function_t *_rehasher = nullptr);
 };
 
 struct SampleRootBuilder {
   flatbuffers::FlatBufferBuilder &fbb_;
   flatbuffers::uoffset_t start_;
-  void add_objects_type(flatbuffers::Offset<flatbuffers::Vector<Object>> objects_type) {
+  void add_objects_type(flatbuffers::Offset<flatbuffers::Vector<uint8_t>> objects_type) {
     fbb_.AddOffset(SampleRoot::VT_OBJECTS_TYPE, objects_type);
   }
   void add_objects(flatbuffers::Offset<flatbuffers::Vector<flatbuffers::Offset<void>>> objects) {
@@ -302,7 +292,7 @@ struct SampleRootBuilder {
 
 inline flatbuffers::Offset<SampleRoot> CreateSampleRoot(
     flatbuffers::FlatBufferBuilder &_fbb,
-    flatbuffers::Offset<flatbuffers::Vector<Object>> objects_type = 0,
+    flatbuffers::Offset<flatbuffers::Vector<uint8_t>> objects_type = 0,
     flatbuffers::Offset<flatbuffers::Vector<flatbuffers::Offset<void>>> objects = 0) {
   SampleRootBuilder builder_(_fbb);
   builder_.add_objects(objects);
@@ -312,104 +302,12 @@ inline flatbuffers::Offset<SampleRoot> CreateSampleRoot(
 
 inline flatbuffers::Offset<SampleRoot> CreateSampleRootDirect(
     flatbuffers::FlatBufferBuilder &_fbb,
-    const std::vector<Object> objects_type,
+    const std::vector<uint8_t> *objects_type = nullptr,
     const std::vector<flatbuffers::Offset<void>> *objects = nullptr) {
   return sample::CreateSampleRoot(
       _fbb,
-      !objects_type.empty() ? _fbb.CreateVector<Object>(objects_type) : Object_NONE,
+      objects_type ? _fbb.CreateVector<uint8_t>(*objects_type) : 0,
       objects ? _fbb.CreateVector<flatbuffers::Offset<void>>(*objects) : 0);
-}
-
-flatbuffers::Offset<SampleRoot> CreateSampleRoot(flatbuffers::FlatBufferBuilder &_fbb, const SampleRootT *_o, const flatbuffers::rehasher_function_t *_rehasher = nullptr);
-
-inline Object1T *Object1::UnPack(const flatbuffers::resolver_function_t *_resolver) const {
-  auto _o = new Object1T();
-  UnPackTo(_o, _resolver);
-  return _o;
-}
-
-inline void Object1::UnPackTo(Object1T *_o, const flatbuffers::resolver_function_t *_resolver) const {
-  (void)_o;
-  (void)_resolver;
-  { auto _e = text(); if (_e) _o->text = _e->str(); };
-  { auto _e = boolean(); _o->boolean = _e; };
-}
-
-inline flatbuffers::Offset<Object1> Object1::Pack(flatbuffers::FlatBufferBuilder &_fbb, const Object1T* _o, const flatbuffers::rehasher_function_t *_rehasher) {
-  return CreateObject1(_fbb, _o, _rehasher);
-}
-
-inline flatbuffers::Offset<Object1> CreateObject1(flatbuffers::FlatBufferBuilder &_fbb, const Object1T *_o, const flatbuffers::rehasher_function_t *_rehasher) {
-  (void)_rehasher;
-  (void)_o;
-  auto _text = _fbb.CreateString(_o->text);
-  auto _boolean = _o->boolean;
-  return sample::CreateObject1(
-      _fbb,
-      _text,
-      _boolean);
-}
-
-inline Object2T *Object2::UnPack(const flatbuffers::resolver_function_t *_resolver) const {
-  auto _o = new Object2T();
-  UnPackTo(_o, _resolver);
-  return _o;
-}
-
-inline void Object2::UnPackTo(Object2T *_o, const flatbuffers::resolver_function_t *_resolver) const {
-  (void)_o;
-  (void)_resolver;
-  { auto _e = text(); if (_e) _o->text = _e->str(); };
-  { auto _e = integer(); _o->integer = _e; };
-}
-
-inline flatbuffers::Offset<Object2> Object2::Pack(flatbuffers::FlatBufferBuilder &_fbb, const Object2T* _o, const flatbuffers::rehasher_function_t *_rehasher) {
-  return CreateObject2(_fbb, _o, _rehasher);
-}
-
-inline flatbuffers::Offset<Object2> CreateObject2(flatbuffers::FlatBufferBuilder &_fbb, const Object2T *_o, const flatbuffers::rehasher_function_t *_rehasher) {
-  (void)_rehasher;
-  (void)_o;
-  auto _text = _fbb.CreateString(_o->text);
-  auto _integer = _o->integer;
-  return sample::CreateObject2(
-      _fbb,
-      _text,
-      _integer);
-}
-
-inline SampleRootT *SampleRoot::UnPack(const flatbuffers::resolver_function_t *_resolver) const {
-  auto _o = new SampleRootT();
-  UnPackTo(_o, _resolver);
-  return _o;
-}
-
-inline void SampleRoot::UnPackTo(SampleRootT *_o, const flatbuffers::resolver_function_t *_resolver) const {
-  (void)_o;
-  (void)_resolver;
-  { auto _e = objects_type(); if (_e) { _o->objects_type.resize(_e->size()); for (flatbuffers::uoffset_t _i = 0; _i < _e->size(); _i++) { _o->objects_type[_i] = static_cast<Object>(_e->Get(_i)); } } };
-  { auto _e = objects(); if (_e) { _o->objects.resize(_e->size()); for (flatbuffers::uoffset_t _i = 0; _i < _e->size(); _i++) { _o->objects[_i].table = ObjectUnion::UnPack(_e->Get(_i), static_cast<Object>(objects_type()->Get(_i)),_resolver); } } };
-}
-
-inline flatbuffers::Offset<SampleRoot> SampleRoot::Pack(flatbuffers::FlatBufferBuilder &_fbb, const SampleRootT* _o, const flatbuffers::rehasher_function_t *_rehasher) {
-  return CreateSampleRoot(_fbb, _o, _rehasher);
-}
-
-inline flatbuffers::Offset<SampleRoot> CreateSampleRoot(flatbuffers::FlatBufferBuilder &_fbb, const SampleRootT *_o, const flatbuffers::rehasher_function_t *_rehasher) {
-  (void)_rehasher;
-  (void)_o;
-  auto _objects_type = _o->objects_type.size() ? _fbb.CreateVector((const Object*)_o->objects_type.data(), _o->objects_type.size()) : 0;
-  //std::vector<unsigned char*>  _objects; _objects.resize(_o->objects.size());
-  std::vector<flatbuffers::Offset<void>>  _objects; _objects.resize(_o->objects.size());
-  for(auto _i = 0; _i < _objects.size(); ++_i){
-     _objects[_i] = _o->objects[_i].Pack(_fbb);
-  }
-  //auto _objects = _o->objects.size() ? _fbb.CreateVector((const unsigned char*)_o->objects.data(), _o->objects.size()) : 0;
-  return sample::CreateSampleRoot(
-      _fbb,
-      _objects_type,
-      _fbb.CreateVector<flatbuffers::Offset<void>>(_objects)
-  );
 }
 
 inline bool VerifyObject(flatbuffers::Verifier &verifier, const void *obj, Object type) {
@@ -423,6 +321,10 @@ inline bool VerifyObject(flatbuffers::Verifier &verifier, const void *obj, Objec
     }
     case Object_Object2: {
       auto ptr = reinterpret_cast<const Object2 *>(obj);
+      return verifier.VerifyTable(ptr);
+    }
+    case Object_Object3: {
+      auto ptr = reinterpret_cast<const Object3 *>(obj);
       return verifier.VerifyTable(ptr);
     }
     default: return false;
@@ -440,52 +342,6 @@ inline bool VerifyObjectVector(flatbuffers::Verifier &verifier, const flatbuffer
   return true;
 }
 
-inline flatbuffers::NativeTable *ObjectUnion::UnPack(const void *obj, Object type, const flatbuffers::resolver_function_t *resolver) {
-  switch (type) {
-    case Object_Object1: {
-      auto ptr = reinterpret_cast<const Object1 *>(obj);
-      return ptr->UnPack(resolver);
-    }
-    case Object_Object2: {
-      auto ptr = reinterpret_cast<const Object2 *>(obj);
-      return ptr->UnPack(resolver);
-    }
-    default: return nullptr;
-  }
-}
-
-inline flatbuffers::Offset<void> ObjectUnion::Pack(flatbuffers::FlatBufferBuilder &_fbb, const flatbuffers::rehasher_function_t *_rehasher) const {
-  switch (type) {
-    case Object_Object1: {
-      auto ptr = reinterpret_cast<const Object1T *>(table);
-      return CreateObject1(_fbb, ptr, _rehasher).Union();
-    }
-    case Object_Object2: {
-      auto ptr = reinterpret_cast<const Object2T *>(table);
-      return CreateObject2(_fbb, ptr, _rehasher).Union();
-    }
-    default: return 0;
-  }
-}
-
-inline void ObjectUnion::Reset() {
-  switch (type) {
-    case Object_Object1: {
-      auto ptr = reinterpret_cast<Object1T *>(table);
-      delete ptr;
-      break;
-    }
-    case Object_Object2: {
-      auto ptr = reinterpret_cast<Object2T *>(table);
-      delete ptr;
-      break;
-    }
-    default: break;
-  }
-  table = nullptr;
-  type = Object_NONE;
-}
-
 inline const sample::SampleRoot *GetSampleRoot(const void *buf) {
   return flatbuffers::GetRoot<sample::SampleRoot>(buf);
 }
@@ -499,12 +355,6 @@ inline void FinishSampleRootBuffer(
     flatbuffers::FlatBufferBuilder &fbb,
     flatbuffers::Offset<sample::SampleRoot> root) {
   fbb.Finish(root);
-}
-
-inline std::unique_ptr<SampleRootT> UnPackSampleRoot(
-    const void *buf,
-    const flatbuffers::resolver_function_t *res = nullptr) {
-  return std::unique_ptr<SampleRootT>(GetSampleRoot(buf)->UnPack(res));
 }
 
 }  // namespace sample
