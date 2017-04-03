6c1beeaa6fb9d47f9531b39a98d33123b303b6dd
MizukiSonoko <mizuki.sonoko@gmail.com>
2017-04-04 00:47:47 +0900

    Update make command suto

--- install-flatbuf.sh
+++ install-flatbuf.sh
@@ -1,2 +1,2 @@
 cd /tmp && git clone https://github.com/google/flatbuffers.git
-cd /tmp/flatbuffers && cmake -G "Unix Makefiles" && make && make install
+cd /tmp/flatbuffers && cmake -G "Unix Makefiles" && make && sudo make install
